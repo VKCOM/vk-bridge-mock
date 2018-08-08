cc5ac870da0bb8d25e8521a8f57a3b416560673a
kuhel <vorontsov.gleb@gmail.com>
2018-08-08 19:20:35 +0300

    Fixes for npm

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,16 @@
 // rollup.config.js
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import babelrc from 'babelrc-rollup';
 
 export default {
   input: 'src/index.js',
   output: {
     file: 'lib/index.js',
-    format: 'umd',
-    name: 'vk-connect-mock',
-    exports: 'named',
+    format: 'esm'
   },
   plugins: [
     resolve(),
-    babel({
-      exclude: 'node_modules/**' // only transpile our source code
-    })
+    babel(babelrc())
   ]
 };

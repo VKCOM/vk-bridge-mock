6e0836a4d2712df74b3bb242b39bf9ee81872153
denismart <denismart87@gmail.com>
2018-09-28 21:23:48 +0300

    Update VKWebAppGetUserInfo.js
    add missed field `id` to userData

--- src/Data/VKWebAppGetUserInfo.js
+++ src/Data/VKWebAppGetUserInfo.js
@@ -1,6 +1,7 @@
 export const userData = {
   type: 'VKWebAppGetUserInfoResult',
   data: {
+    id: 100500,
     photo_100: 'https://pp.userapi.com/c841034/v841034569/3b8c1/pt3sOw_qhfg.jpg',
     photo_200: 'https://pp.userapi.com/c841034/v841034569/3b8c1/pt3sOw_qhfg.jpg',
     first_name: 'Karl Heinrich',

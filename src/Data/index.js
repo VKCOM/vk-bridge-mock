428d0c4b3e4b90ec20fc8d97661fe57cc2976729
Evgeny Ustinkin <evgeny@ustinkin.com>
2018-12-03 23:47:28 +0300

    Added Default mock, and throw error if mock does not exists

--- src/Data/index.js
+++ src/Data/index.js
@@ -1,66 +1,66 @@
-import { userData, userDataError } from './VKWebAppGetUserInfo';
-import { initData, initDataError } from './VKWebAppInit';
-import { authData, authDataError } from './VKWebAppGetAuthToken';
-import { apiDataError, apiData } from './VKWebAppCallAPIMethod';
-import { geoData, geoDataError } from './VKWebAppGetGeodata';
-import { phoneData, phoneDataError } from './VKWebAppGetPhoneNumber';
-import { clientData, clientDataError } from './VKWebAppGetClientVersion';
-import { emailData, emailDataError } from './VKWebAppGetEmail';
+import {userData, userDataError} from './VKWebAppGetUserInfo';
+import {initData, initDataError} from './VKWebAppInit';
+import {authData, authDataError} from './VKWebAppGetAuthToken';
+import {apiDataError, apiData} from './VKWebAppCallAPIMethod';
+import {geoData, geoDataError} from './VKWebAppGetGeodata';
+import {phoneData, phoneDataError} from './VKWebAppGetPhoneNumber';
+import {clientData, clientDataError} from './VKWebAppGetClientVersion';
+import {emailData, emailDataError} from './VKWebAppGetEmail';
 
 class DataModel {
-  constructor(data, errorData) {
-    this.mockedData = data;
-    this.mockedErrorData = errorData;
-  }
+	constructor(data, errorData) {
+		this.mockedData = data;
+		this.mockedErrorData = errorData;
+	}
 
-  set data(data) {
-    this.mockedData = data;
-  }
+	set data(data) {
+		this.mockedData = data;
+	}
 
-  get data() {
-    return this.mockedData;
-  }
+	get data() {
+		return this.mockedData;
+	}
 
-  set errorData(data) {
-    this.mockedErrorData = data;
-  }
+	set errorData(data) {
+		this.mockedErrorData = data;
+	}
 
-  get errorData() {
-    return this.mockedErrorData;
-  }
+	get errorData() {
+		return this.mockedErrorData;
+	}
 }
 
 export default {
-  // Data
-  VKWebAppInit: new DataModel(initData, initDataError),
-  // Data
-  VKWebAppGetAuthToken: new DataModel(authData, authDataError),
-  // Data
-  VKWebAppCallAPIMethod: new DataModel(apiData, apiDataError),
-  // Data
-  VKWebAppGetGeodata: new DataModel(geoData, geoDataError),
-  // Datab
-  VKWebAppGetUserInfo: new DataModel(userData, userDataError),
-  // Data
-  VKWebAppGetPhoneNumber: new DataModel(phoneData, phoneDataError),
-  // Data
-  VKWebAppGetClientVersion: new DataModel(clientData, clientDataError),
-  // Data
-  VKWebAppGetEmail: new DataModel(emailData, emailDataError),
-  // TODO UI
-  VKWebAppOpenPayForm: {},
-  // TODO UI
-  VKWebAppShare: {},
-  // TODO UI
-  VKWebAppAllowNotifications: {},
-  // TODO UI
-  VKWebAppDenyNotifications: {},
-  // TODO UI
-  VKWebAppShowWallPostBox: {},
-  // TODO ???
-  VKWebAppSetLocation: {},
-  // TODO UI
-  VKWebAppAllowMessagesFromGroup: {},
-  // TODO UI
-  VKWebAppJoinGroup: {},
+	// Data
+	VKWebAppInit: new DataModel(initData, initDataError),
+	// Data
+	VKWebAppGetAuthToken: new DataModel(authData, authDataError),
+	// Data
+	VKWebAppCallAPIMethod: new DataModel(apiData, apiDataError),
+	// Data
+	VKWebAppGetGeodata: new DataModel(geoData, geoDataError),
+	// Datab
+	VKWebAppGetUserInfo: new DataModel(userData, userDataError),
+	// Data
+	VKWebAppGetPhoneNumber: new DataModel(phoneData, phoneDataError),
+	// Data
+	VKWebAppGetClientVersion: new DataModel(clientData, clientDataError),
+	// Data
+	VKWebAppGetEmail: new DataModel(emailData, emailDataError),
+	// TODO UI
+	VKWebAppOpenPayForm: {},
+	// TODO UI
+	VKWebAppShare: {},
+	// TODO UI
+	VKWebAppAllowNotifications: {},
+	// TODO UI
+	VKWebAppDenyNotifications: {},
+	// TODO UI
+	VKWebAppShowWallPostBox: {},
+	// TODO ???
+	VKWebAppSetLocation: {},
+	// TODO UI
+	VKWebAppAllowMessagesFromGroup: {},
+	// TODO UI
+	VKWebAppJoinGroup: {}
 };
--- src/Handlers/index.js
+++ src/Handlers/index.js
@@ -6,37 +6,38 @@ import GetGeodata from './VKWebAppGetGeodata';
 import GetPhoneNumber from './VKWebAppGetPhoneNumber';
 import GetClientVersion from './VKWebAppGetClientVersion';
 import GetEmail from './VKWebAppGetEmail';
+import Default from './Default';
 
 export default {
-  VKWebAppInit: Init(),
-  // Data
-  VKWebAppGetAuthToken: GetAuthToken(),
-  // Data
-  VKWebAppCallAPIMethod: CallAPIMethod(),
-  // Data
-  VKWebAppGetGeodata: GetGeodata(),
-  // Datab
-  VKWebAppGetUserInfo: GetUserInfo(),
-  // Data
-  VKWebAppGetPhoneNumber: GetPhoneNumber(),
-  // Data
-  VKWebAppGetClientVersion: GetClientVersion(),
-  // Data
-  VKWebAppGetEmail: GetEmail(),
-  // TODO UI
-  VKWebAppOpenPayForm: {},
-  // TODO UI
-  VKWebAppShare: {},
-  // TODO UI
-  VKWebAppAllowNotifications: {},
-  // TODO UI
-  VKWebAppDenyNotifications: {},
-  // TODO UI
-  VKWebAppShowWallPostBox: {},
-  // TODO ???
-  VKWebAppSetLocation: {},
-  // TODO UI
-  VKWebAppAllowMessagesFromGroup: {},
-  // TODO UI
-  VKWebAppJoinGroup: {},
+	VKWebAppInit: Init(),
+	// Data
+	VKWebAppGetAuthToken: GetAuthToken(),
+	// Data
+	VKWebAppCallAPIMethod: CallAPIMethod(),
+	// Data
+	VKWebAppGetGeodata: GetGeodata(),
+	// Datab
+	VKWebAppGetUserInfo: GetUserInfo(),
+	// Data
+	VKWebAppGetPhoneNumber: GetPhoneNumber(),
+	// Data
+	VKWebAppGetClientVersion: GetClientVersion(),
+	// Data
+	VKWebAppGetEmail: GetEmail(),
+	// TODO UI
+	VKWebAppOpenPayForm: Default("VKWebAppOpenPayForm"),
+	// TODO UI
+	VKWebAppShare: Default("VKWebAppShare"),
+	// TODO UI
+	VKWebAppAllowNotifications: Default("VKWebAppAllowNotifications"),
+	// TODO UI
+	VKWebAppDenyNotifications: Default("VKWebAppDenyNotifications"),
+	// TODO UI
+	VKWebAppShowWallPostBox: Default("VKWebAppShowWallPostBox"),
+	// TODO ???
+	VKWebAppSetLocation: Default("VKWebAppSetLocation"),
+	// TODO UI
+	VKWebAppAllowMessagesFromGroup: Default("VKWebAppAllowMessagesFromGroup"),
+	// TODO UI
+	VKWebAppJoinGroup: Default("VKWebAppJoinGroup")
 };

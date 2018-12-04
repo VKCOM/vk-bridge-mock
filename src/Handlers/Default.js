bbc26ef2a6b942fa71a26f3a7256c7fa1e7bdf5a
Evgeny Ustinkin <evgeny@ustinkin.com>
2018-12-04 18:08:38 +0300

    Fix Tabs

--- src/Handlers/Default.js
+++ src/Handlers/Default.js
@@ -2,18 +2,15 @@ import {response as res} from '../index';
 import VKWebAppEvent from '../VKWebAppEvent';
 
 export default(eventName, hasError = false) => {
-	return {
-		postMessage: (params) => {
-			if (!res[eventName].data) {
-				throw Error(eventName + " data not mocked")
-			}
-			if (!res[eventName].errorData) {
-				throw Error(eventName + " errorData not mocked")
-			}
-			VKWebAppEvent(
-				!hasError
-				? res[eventName].data
-				: res[eventName].errorData);
-		}
-	};
+  return {
+	postMessage: (params) => {
+	  if (!res[eventName].data) {
+	  	throw Error(eventName + " data not mocked")
+	  }
+	  if (!res[eventName].errorData) {
+		throw Error(eventName + " errorData not mocked")
+	  } 
+	  VKWebAppEvent(!hasError ? res[eventName].data : res[eventName].errorData);
+	}
+  };
 };
--- src/Handlers/index.js
+++ src/Handlers/index.js
@@ -9,35 +9,35 @@ import GetEmail from './VKWebAppGetEmail';
 import Default from './Default';
 
 export default {
-	VKWebAppInit: Init(),
-	// Data
-	VKWebAppGetAuthToken: GetAuthToken(),
-	// Data
-	VKWebAppCallAPIMethod: CallAPIMethod(),
-	// Data
-	VKWebAppGetGeodata: GetGeodata(),
-	// Datab
-	VKWebAppGetUserInfo: GetUserInfo(),
-	// Data
-	VKWebAppGetPhoneNumber: GetPhoneNumber(),
-	// Data
-	VKWebAppGetClientVersion: GetClientVersion(),
-	// Data
-	VKWebAppGetEmail: GetEmail(),
-	// TODO UI
-	VKWebAppOpenPayForm: Default("VKWebAppOpenPayForm"),
-	// TODO UI
-	VKWebAppShare: Default("VKWebAppShare"),
-	// TODO UI
-	VKWebAppAllowNotifications: Default("VKWebAppAllowNotifications"),
-	// TODO UI
-	VKWebAppDenyNotifications: Default("VKWebAppDenyNotifications"),
-	// TODO UI
-	VKWebAppShowWallPostBox: Default("VKWebAppShowWallPostBox"),
-	// TODO ???
-	VKWebAppSetLocation: Default("VKWebAppSetLocation"),
-	// TODO UI
-	VKWebAppAllowMessagesFromGroup: Default("VKWebAppAllowMessagesFromGroup"),
-	// TODO UI
-	VKWebAppJoinGroup: Default("VKWebAppJoinGroup")
+ VKWebAppInit: Init(),
+ // Data
+ VKWebAppGetAuthToken: GetAuthToken(),
+ // Data
+ VKWebAppCallAPIMethod: CallAPIMethod(),
+ // Data
+ VKWebAppGetGeodata: GetGeodata(),
+ // Datab
+ VKWebAppGetUserInfo: GetUserInfo(),
+ // Data
+ VKWebAppGetPhoneNumber: GetPhoneNumber(),
+ // Data
+ VKWebAppGetClientVersion: GetClientVersion(),
+ // Data
+ VKWebAppGetEmail: GetEmail(),
+ // TODO UI
+ VKWebAppOpenPayForm: Default("VKWebAppOpenPayForm"),
+ // TODO UI
+ VKWebAppShare: Default("VKWebAppShare"),
+ // TODO UI
+ VKWebAppAllowNotifications: Default("VKWebAppAllowNotifications"),
+ // TODO UI
+ VKWebAppDenyNotifications: Default("VKWebAppDenyNotifications"),
+ // TODO UI
+ VKWebAppShowWallPostBox: Default("VKWebAppShowWallPostBox"),
+ // TODO ???
+ VKWebAppSetLocation: Default("VKWebAppSetLocation"),
+ // TODO UI
+ VKWebAppAllowMessagesFromGroup: Default("VKWebAppAllowMessagesFromGroup"),
+ // TODO UI
+ VKWebAppJoinGroup: Default("VKWebAppJoinGroup")
 };

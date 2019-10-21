c0385db3089292e4d98fa33079b8c45dece0a241
Gleb Vorontsov <vorontsov.gleb@gmail.com>
2019-10-21 11:07:34 +0300

    Token mock

--- src/Data/VKWebAppGetAuthToken.js
+++ src/Data/VKWebAppGetAuthToken.js
@@ -1,7 +1,7 @@
 export const authData = {
   type: 'VKWebAppAccessTokenReceived',
   data: {
-    access_token: '255f511f85047d79cf5aea962e4c5ade60fc89475874ab1efe4fd7552f44245c111850d6bedd6894027e5'
+    access_token: 'yours000access111token222yours000access111token222yours000access111token2220123456789'
   }
 };
 

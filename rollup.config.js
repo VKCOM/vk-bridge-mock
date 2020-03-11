f5301be5d1e091ef6651ea9a9b0da5e7f9e4848b
Danakt Saushkin <danakt@protonmail.com>
2020-03-11 19:54:46 +0300

    Add browser version

--- rollup.config.js
+++ rollup.config.js
@@ -10,6 +10,7 @@ const IS_PROD = process.env.NODE_ENV === 'production';
 
 const INPUT_FILE = './src/index.ts';
 const INPUT_FILE_BROWSER = './src/browser.ts';
+const OUTPUT_FILE_BROWSER = './dist/browser.min.js';
 
 const getPlugins = (tsDeclaration = false) => [
   typescript(
@@ -54,14 +55,24 @@ const es = {
 };
 
 const umd = {
-  plugins: [...getPlugins(), uglify()],
-  input: INPUT_FILE_BROWSER,
+  plugins: getPlugins(),
+  input: INPUT_FILE,
   output: [
     {
+      name: pkg.umdName,
       file: pkg.browser,
       format: 'umd'
     }
   ]
 };
 
-export default IS_PROD ? [cjs, es, umd] : cjs;
+const browser = {
+  plugins: [...getPlugins(), uglify()],
+  input: INPUT_FILE_BROWSER,
+  output: {
+    file: OUTPUT_FILE_BROWSER,
+    format: 'iife'
+  }
+};
+
+export default IS_PROD ? [cjs, es, umd, browser] : cjs;
